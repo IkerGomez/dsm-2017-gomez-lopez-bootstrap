5893bf2c1585862f62b5ad9f361f3ebbecae428a
Cristian Martínez de Morentin Suescun <cristian.martinezdemorentin@gmail.com>
2017-02-23 09:16:18 +0100

    Añadido índice de prácticas

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php header('Location: /Practica02/index.html'); ?>
+<?php
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <title> Prácticas Cristian e Iker | DSM </title>
+</head>
+
+<body>
+    <h1>Índice de prácticas - Cristian e Iker</h1>
+
+    <p><a href="/Practica01/index.html">Práctica 1</a></p>
+    <p><a href="/Practica02/index.html">Práctica 2</a></p>
+</body>
+
+</html>
+
+
+
+?>
