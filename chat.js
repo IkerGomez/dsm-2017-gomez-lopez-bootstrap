b48c02b3bfa4eb00e95963f3d1c7f45ba28a262d
Cristian Martínez de Morentin Suescun <cristian.martinezdemorentin@gmail.com>
2017-04-06 17:22:39 +0200

    Heroku test

--- chat.js
+++ chat.js
@@ -32,13 +32,13 @@ var Mensaje = mongoose.model('Mensaje', Mensaje);
 
 function connecToMongoDB()
 {
-    mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://dsm:marko_dsm_2017@ds139370.mlab.com:39370/chat-dsm', { server: { auto_reconnect: true } }, function (err) {
+    mongoose.connect(process.env.MONGOLAB_URI || '://dsm:marko_dsm_2017@ds139370.mlab.com:39370/chat-dsm', { server: { auto_reconnect: true } }, function (err) {
         if(!err)
         {
             console.log("Conectado a la base de datos");
         } else
         {
-            throw err;
+            //throw err;
         }
     });
 }
